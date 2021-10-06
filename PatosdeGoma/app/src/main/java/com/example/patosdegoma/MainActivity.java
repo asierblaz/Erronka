ed1ee00caa8141af5055d329caa44919de830011
Asier <blazgar@gmail.com>
2021-10-06 10:34:21 +0200

    color

--- PatosdeGoma/app/src/main/java/com/example/patosdegoma/MainActivity.java
+++ PatosdeGoma/app/src/main/java/com/example/patosdegoma/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,8 +20,7 @@ public class MainActivity extends AppCompatActivity {
         //poner icono en action bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
-        getSupportActionBar().setTitle("o");
-
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#b99932'>Patinhos Gomosos </font>"));
         //Carga csv a produktuak
         Produktua.fileToArraylist(this);
         Produktua.categoriasToArray();
